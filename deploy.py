118b1959b0642e16b209f77b4bb3d3c9fd8b0c51
Reme <remeajayi@gmail.com>
2021-08-18 18:32:10 -0400

    finished predictions

---
+++ deploy.py
@@ -0,0 +1,22 @@
+"""
+Deploy skill extraction App to Streamlit
+Author: Olohireme Ajayi
+"""
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+"""
+project title
+Explanation
+Where did i find the data
+
+feature generation
+model selection
+plots
+"""
+
+header = st.beta_container()
+
+with header:
+    st.Title('Skill Extraction with LSTM')
